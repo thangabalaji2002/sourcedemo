38b704035b993c09dce768ff15a7b717be158b7d
DESKTOP-2B0A2N5\Thanga Balaji <thangabalaji1812002@gmail.com>
2023-08-15 20:53:21 +0530

    first program in c #

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using static ConsoleApp2.Program;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+       
+       
+       
+
+        static void Main(string[] args)
+        {
+
+
+
+            Debug.WriteLine("Hello world");
+
+
+        }
+
+        }
+     
+  
+}
+
